867da09956da1397bd33f82a4cb637490c2bbda0
hantke@xray.bmc.uu.se <max.hantke@stud.uni-goettingen.de>
2013-11-10 10:49:02 +0100

    cmap propblems with the light maps

--- colormaptools.py
+++ colormaptools.py
@@ -30,7 +30,7 @@ cdict_jet = {'red':   [(0.0,0.0,0.0),
                        (0.625,0.0,0.0),
                        (1.0,0.0,0.0)]}
 
-cmaps["myjet"] = matplotlib.colors.LinearSegmentedColormap('myjet', cdict_jet, 1024)}
+cmaps["myjet"] = matplotlib.colors.LinearSegmentedColormap('myjet', cdict_jet, 1024)
 
 cdict_jet_lightbg = {'red':   [(0.0,1.0,1.0),
                                (0.125,0.0,0.0),
@@ -52,7 +52,7 @@ cdict_jet_lightbg = {'red':   [(0.0,1.0,1.0),
                                (0.625,0.0,0.0),
                                (1.0,0.0,0.0)]}
 
-cmaps["jet_lightbg"] = matplotlib.colors.LinearSegmentedColormap('jet_lightbg', cdict_jet_lightbg, 1024)}
+cmaps["jet_lightbg"] = matplotlib.colors.LinearSegmentedColormap('jet_lightbg', cdict_jet_lightbg, 1024)
 
 Nal = 4.
 al = [0.,0/Nal,1/Nal,2/Nal,3/Nal,4/Nal]
@@ -77,7 +77,7 @@ cdict_jet_lightbg2 = {'red':   [(al[0],1.0,1.0),
                                 (al[4],0.0,0.0),
                                 (al[5],0.0,0.0)]}
 
-cmaps["jet_lightbg2"] = matplotlib.colors.LinearSegmentedColormap('jet_lightbg2', cdict_jet_lightbg2, 1024)}
+cmaps["jet_lightbg2"] = matplotlib.colors.LinearSegmentedColormap('jet_lightbg2', cdict_jet_lightbg2, 1024)
 
 def make_colorbar(filename,Nx,Ny,colormap=cm.jet,orientation="vertical"):
     X,Y = meshgrid(arange(Nx),arange(Ny))
