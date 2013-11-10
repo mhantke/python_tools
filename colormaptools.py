cb22b6a565f60d9a96e469ece1f5f953d9a1c0c1
Max Hantke <maxhantke@gmail.com>
2013-11-10 19:36:38 +0100

    new cyclic colormap called redblue

--- colormaptools.py
+++ colormaptools.py
@@ -54,30 +54,53 @@ cdict_jet_lightbg = {'red':   [(0.0,1.0,1.0),
 
 cmaps["jet_lightbg"] = matplotlib.colors.LinearSegmentedColormap('jet_lightbg', cdict_jet_lightbg, 1024)
 
-Nal = 4.
-al = [0.,0/Nal,1/Nal,2/Nal,3/Nal,4/Nal]
-cdict_jet_lightbg2 = {'red':   [(al[0],1.0,1.0),
-                                (al[1],0.0,0.0),
-                                (al[2],0.0,0.0),
-                                (al[3],1.0,1.0),
-                                (al[4],1.0,1.0),
-                                (al[5],0.5,0.5)],
+
+cdict_jet_lightbg2 = {'red':   [(0.0,1.0,1.0),
+                                (0.05,0.8,0.8),
+                                (0.225,0.0,0.0),
+                                (0.375,0.0,0.0),
+                                (0.625,1.0,1.0),
+                                (0.875,1.0,1.0),
+                                (1.0,0.5,0.5)],
                      
-                      'green':  [(al[0],1.0,1.0),
-                                 (al[1],0.0,0.0),
-                                 (al[2],1.0,1.0),
-                                 (al[3],1.0,1.0),
-                                 (al[4],0.0,0.0),
-                                 (al[5],0.0,0.0)],
+                      'green':  [(0.0,1.0,1.0),
+                                 (0.05,0.8,0.8),
+                                 (0.225,0.0,0.0),
+                                 (0.375,1.0,1.0),
+                                 (0.625,1.0,1.0),
+                                 (0.875,0.0,0.0),
+                                 (1.0,0.0,0.0)],
                      
-                      'blue':  [(al[0],1.0,1.0),
-                                (al[1],1.0,1.0),
-                                (al[2],1.0,1.0),
-                                (al[3],0.0,0.0),
-                                (al[4],0.0,0.0),
-                                (al[5],0.0,0.0)]}
+                      'blue':  [(0.0,1.0,1.0),
+                                (0.05,1.0,1.0),
+                                (0.225,1.0,1.0),
+                                (0.375,1.0,1.0),
+                                (0.625,0.0,0.0),
+                                (1.0,0.0,0.0)]}
 
 cmaps["jet_lightbg2"] = matplotlib.colors.LinearSegmentedColormap('jet_lightbg2', cdict_jet_lightbg2, 1024)
+
+cdict_redblue = {'red':   [(0.00,0.0,0.0),
+                           (0.25,1.0,1.0),
+                           (0.50,1.0,1.0),
+                           (0.75,0.0,0.0),
+                           (1.00,0.0,0.0)],
+                     
+                 'green':  [(0.00,0.0,0.0),
+                            (0.25,0.0,0.0),
+                            (0.50,1.0,1.0),
+                            (0.75,0.0,0.0),
+                            (1.00,0.0,0.0)],
+                 
+                 'blue':  [(0.00,0.0,0.0),
+                           (0.25,0.0,0.0),
+                           (0.50,1.0,1.0),
+                           (0.75,1.0,1.0),
+                           (1.00,0.0,0.0)]}
+
+cmaps["redblue"] = matplotlib.colors.LinearSegmentedColormap('redblue', cdict_redblue, 1024)
+
+
 
 def make_colorbar(filename,Nx,Ny,colormap=cm.jet,orientation="vertical"):
     X,Y = meshgrid(arange(Nx),arange(Ny))
